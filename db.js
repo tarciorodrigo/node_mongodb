d86aad9d864a63675b59e99a9a136646b8ba7afa
tarciorodrigo <tarciorodrigoa@gmail.com>
2025-05-19 17:28:51 -0300

    03

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 async function connect() {
   const { MongoClient } = require("mongodb");
-  const client = new MongoClient("mongodb://localhost:27017");
+  const client = new MongoClient("mongodb://admin:admin123@localhost:27017");
   await client.connect();
   global.connection = client.db("aula2");
   console.log("Connected to MongoDB");
@@ -8,4 +8,19 @@ async function connect() {
 
 connect();
 
-module.exports = {};
+// function findCustomers(callback) {
+//   return global.connection
+//     .collection("customers")
+//     .find({})
+//     .toArray((error, docs) => {
+//       callback(error, docs);
+//     });
+// }
+
+function findCustomers() {
+  return global.connection.collection("customers").find({}).toArray();
+}
+
+module.exports = {
+  findCustomers,
+};
